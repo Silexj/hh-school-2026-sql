52c8d95542f9f2c40e7e9e6ed46a871a538bc8b4
Danil.S <silex2dev@gmail.com>
2026-03-09 21:35:10 +0700

    Fixed query logic

--- task-3.sql
+++ task-3.sql
@@ -1,5 +1,7 @@
 SELECT a.area_id, a.area_name,
-       round(AVG((v.compensation_from + v.compensation_to) / 2), 2) AS avg_,
+       round(AVG(COALESCE((v.compensation_from + v.compensation_to) / 2.0,
+                            v.compensation_from,
+                            v.compensation_to)), 2) AS avg_from_to,
        round(AVG(v.compensation_from), 2) AS avg_from,
        round(AVG(v.compensation_to), 2) AS avg_to
 FROM vacancy v
